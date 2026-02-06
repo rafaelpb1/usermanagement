f4d52d8eb19ba98c5123eacf422d9a4e8d3b08f7
Rafael <rafaelluna10@hotmail.com>
2026-02-06 09:45:11 -0300

    Rotas de exclusão e listagem

--- src/main/java/com/example/usermanagement/controller/VehicleController.java
+++ src/main/java/com/example/usermanagement/controller/VehicleController.java
@@ -8,10 +8,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/vehicles")
@@ -25,5 +24,18 @@ public class VehicleController {
     public ResponseEntity<VehicleResponseDTO> createVehicle(@RequestBody @Valid VehicleRequestDTO dto) {
         VehicleResponseDTO vehicle = service.create(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(vehicle);
+    }
+
+    @DeleteMapping("/{vin}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> deleteVehicleByVin(@PathVariable String vin) {
+        service.deleteByVIN(vin);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    public ResponseEntity<List<VehicleResponseDTO>> listAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(service.listAllVehicles());
     }
 }
