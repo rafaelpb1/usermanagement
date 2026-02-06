5ffc196e353864f701a35a61c74e4ec3d1d94479
Rafael <rafaelluna10@hotmail.com>
2026-02-06 09:44:49 -0300

    Adicionando métodos de listar e excluir

--- src/main/java/com/example/usermanagement/service/VehicleService.java
+++ src/main/java/com/example/usermanagement/service/VehicleService.java
@@ -2,6 +2,8 @@ package com.example.usermanagement.service;
 
 import com.example.usermanagement.dto.VehicleRequestDTO;
 import com.example.usermanagement.dto.VehicleResponseDTO;
+import com.example.usermanagement.exception.NotFoundVinException;
+import com.example.usermanagement.exception.RoleBusinessException;
 import com.example.usermanagement.mappers.VehicleMapper;
 import com.example.usermanagement.model.vehicle.Vehicle;
 import com.example.usermanagement.repository.VehicleRepository;
@@ -10,6 +12,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,7 +25,7 @@ public class VehicleService {
     @Transactional
     public VehicleResponseDTO create(VehicleRequestDTO dto) {
         if (dto.vin() != null && repository.existsById(dto.vin())) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT,
+            throw new RoleBusinessException(HttpStatus.CONFLICT,
                     "Already exists car registered with this VIN.");
         }
 
@@ -29,5 +33,23 @@ public class VehicleService {
         Vehicle saved = repository.save(vehicle);
 
         return mapper.toDTO(saved);
+    }
+
+    @Transactional
+    public void deleteByVIN(String vin) {
+        var vinexclude = repository.findById(vin)
+                .orElseThrow( () -> new NotFoundVinException(HttpStatus.NOT_FOUND, "Not found VIN."));
+
+        repository.delete(vinexclude);
+    }
+
+    public List<VehicleResponseDTO> listAllVehicles() {
+        List<Vehicle> all = repository.findAll();
+        List<VehicleResponseDTO> list = all.stream().map(mapper::toDTO).toList();
+
+        if (all.isEmpty()) throw new ResponseStatusException
+                (HttpStatus.NO_CONTENT, "No vehicle found.");
+
+        return list;
     }
 }
