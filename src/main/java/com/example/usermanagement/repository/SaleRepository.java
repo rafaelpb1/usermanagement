feb36fdbce0836ec6903bf6da25b4ae4e9dd36e3
Rafael <rafaelluna10@hotmail.com>
2026-02-11 08:31:23 -0300

    Ajuste no repository

--- src/main/java/com/example/usermanagement/repository/SaleRepository.java
+++ src/main/java/com/example/usermanagement/repository/SaleRepository.java
@@ -4,11 +4,13 @@ import com.example.usermanagement.model.sale.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
-    List<Sale> findByEmployeeId(String document);
+    Optional<Sale> findByEmployeeId(String id);
 
-    boolean existsByVehicleVin(String s);
+    Optional<Sale> findByVehicleVin(String vin);
+
+    Optional<Sale> findByCustomerDocument(String document);
 }
