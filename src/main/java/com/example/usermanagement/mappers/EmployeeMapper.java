bea43d3e6174943e62aa3be99a6bd62c316276c0
Rafael <rafaelluna10@hotmail.com>
2026-02-11 08:27:27 -0300

    Mapeando campo createAt

--- src/main/java/com/example/usermanagement/mappers/EmployeeMapper.java
+++ src/main/java/com/example/usermanagement/mappers/EmployeeMapper.java
@@ -4,11 +4,13 @@ import com.example.usermanagement.dto.EmployeeRequestDTO;
 import com.example.usermanagement.dto.EmployeeResponseDTO;
 import com.example.usermanagement.model.employee.Employee;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface EmployeeMapper {
 
     Employee toEntity(EmployeeRequestDTO dto);
 
+    @Mapping(source = "createdAt", target = "created_at")
     EmployeeResponseDTO toDTO(Employee entity);
 }
