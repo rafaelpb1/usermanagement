8a30bd65f65b07b89cb7727665662723e223964c
Rafael <rafaelluna10@hotmail.com>
2026-02-11 08:21:02 -0300

    Ajustando entity Sale

--- src/main/java/com/example/usermanagement/model/sale/Sale.java
+++ src/main/java/com/example/usermanagement/model/sale/Sale.java
@@ -8,39 +8,44 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.Instant;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "sales")
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Sale {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "vehicle_vin", referencedColumnName = "vin", nullable = false, unique = true)
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "vehicle_vin", nullable = false, unique = true)
     private Vehicle vehicle;
 
-    @ManyToOne
-    @JoinColumn(name = "customer_id", referencedColumnName = "document", nullable = false)
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "customer_id", nullable = false)
     private Customer customer;
 
-    @ManyToOne
-    @JoinColumn(name = "employee_id", referencedColumnName = "id", nullable = false)
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "employee_id", nullable = false)
     private Employee employee;
-
-    @Column(name = "sale_date")
-    private Instant saleDate;
 
     @Column(name = "sale_value", precision = 12, scale = 2, nullable = false)
     private BigDecimal saleValue;
 
-    @Column(name = "payment_method")
-    private String paymentMethod;
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @CreationTimestamp
+    @Column(name = "sale_date")
+    private LocalDateTime saleDate;
 }
