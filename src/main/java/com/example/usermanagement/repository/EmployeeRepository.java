d00124f41866214e616648e1af7edb4cc1fdd1d2
Rafael <rafaelluna10@hotmail.com>
2026-02-11 08:17:45 -0300

    Modificando repositories

--- src/main/java/com/example/usermanagement/repository/EmployeeRepository.java
+++ src/main/java/com/example/usermanagement/repository/EmployeeRepository.java
@@ -4,6 +4,11 @@ import com.example.usermanagement.model.employee.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    boolean existsByName(String name);
+
 }
--- src/main/java/com/example/usermanagement/repository/VehicleRepository.java
+++ src/main/java/com/example/usermanagement/repository/VehicleRepository.java
@@ -6,11 +6,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, String> {
 
     List<Vehicle> findByStatus(VehicleStatus status);
+
+    Optional<Vehicle> findByVin(String vin);
 
     boolean existsByVin(String vin);
 }
