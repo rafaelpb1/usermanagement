8b67b3c5b93900b2b26c8fb9a458979e517e93e9
Rafael <rafael.souza05@cs.unipe.edu.br>
2026-02-25 12:40:34 -0300

    modificando de Enum de notblank para notnull

--- src/main/java/com/example/usermanagement/dto/RegisterDTO.java
+++ src/main/java/com/example/usermanagement/dto/RegisterDTO.java
@@ -2,12 +2,13 @@ package com.example.usermanagement.dto;
 
 import com.example.usermanagement.model.users.Role;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record RegisterDTO(
         @NotBlank(message = "Login is required")
         String login,
         @NotBlank(message = "Password is required")
         String password,
-        @NotBlank(message = "Role is required")
+        @NotNull(message = "Role is required")
         Role role) {
 }
